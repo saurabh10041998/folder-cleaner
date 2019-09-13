b50779247dc69b848af4fe28bad796f1c0f98d93
Saurabh Shinde <shindesaurabhpict@gmail.com>
2019-09-13 10:38:13 -0700

    just started

---
+++ cleaner.py
@@ -0,0 +1,18 @@
+import time
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+
+class Watcher:
+    directory_to_watch = "C:\Users\a\Downloads"
+    def __init__(self):
+        self.observer = Observer()
+    
+    def run(self):
+        event_handler = Handler()
+        self.observer.schedule(event_handler, self.directory_to_watch, recursive=True)
+
+        self.observer.start()
+
+        try:
+            while True:
